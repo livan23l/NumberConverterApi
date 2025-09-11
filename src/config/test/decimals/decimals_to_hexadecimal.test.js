6c4842bfb9b352b0202103a760be050f71eeddb8
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-09-10 22:29:35 -0600

    Chore: Enhancing decimals tests

--- src/config/test/decimals/decimals_to_hexadecimal.test.js
+++ src/config/test/decimals/decimals_to_hexadecimal.test.js
@@ -44,18 +44,27 @@ export const tests = [
     { input: '8526945', expected: '821C61' },
     { input: '1048575', expected: 'FFFFF' },
     { input: '1048576', expected: '100000' },
-    { input: '986512457898756987565', expected: '357A9C4DB120E8DAAD' },
+    {
+        input: '89789745615648545615109848904894651065489710848974108948604654890',
+        expected: 'DA444BBE89FCE14E04C5920FE7AB534913EA4EC8D27E4B9A1C152A'
+    },
 
     // Big negatives
     { input: '-123456', expected: '-1E240' },
     { input: '-987654', expected: '-F1206' },
     { input: '-1048575', expected: '-FFFFF' },
-    { input: '-1048576', expected: '-100000' },
+    {
+        input: '-99999878989787456456484894231348978156189781654789784561564898798',
+        expected: '-F31613D55231EADFA4237F628C83C8EBB886A71F9889FB5DC1D5EE'
+    },
 
     // Big decimals
     { input: '12345.6789', expected: '3039.ADCC63F141205BC01A36E2EB1' },
     { input: '98765.4321', expected: '181CD.6E9E1B089A027525460AA64C2' },
-    { input:'-.0123456789876543210', expected: '-0.0329161F7FB73C328E4F40B22' },
+    {
+        input: '-.012345678987654321798789789745678976545799999979778899875457897987845456666640',
+        expected: '-0.0329161F7FB73C414A7CDA9ED'
+    },
 
     // Big decimals positives and negatives
     { input: '4294967.295', expected: '418937.4B851EB851EB851EB851EB851' },
