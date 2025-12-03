6caab33eb3aab32df0cbde69b2f6d84d133f64ac
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-12-03 13:43:34 -0600

    WIP: Adding marks for the active aside option

--- public/js/Documentation.js
+++ public/js/Documentation.js
@@ -1,6 +1,7 @@
 class Documentation {
     /**
      * Controls the visible section change by pressing the corresponding button.
+     * This method also show the corresponding aside option as active.
      * 
      * @private
      * @returns {void}
@@ -8,7 +9,9 @@ class Documentation {
     #handleSwitchSection() {
         // Get the hidden class and the oppen section
         const hiddenClass = 'main__section--hidden';
+        const activeClass = 'aside__option--active';
         let $oppenSection = document.querySelector('#getting-started');
+        let $oppenSectionBtn = document.querySelector(`.${activeClass}`);
 
         // Get all the sections buttons
         const $btns = document.querySelectorAll('[data-button-for]');
@@ -18,17 +21,23 @@ class Documentation {
             // Get the corresponding section
             const sectionId = $btn.dataset.buttonFor;
             const $section = document.querySelector(`#${sectionId}`);
+            const $sectionBtn = document.querySelector(`#btn-${sectionId}`);
 
             // Add the click event
             $btn.addEventListener('click', () => {
-                // Hide the last open section
+                // Hide the last open section and the corresponding aside option
                 $oppenSection.classList.add(hiddenClass);
+                $oppenSectionBtn.classList.remove(activeClass);
 
                 // Show the new section
                 $section.classList.remove(hiddenClass);
 
-                // Update the last open section
+                // Active the corresponding aside option
+                $sectionBtn.classList.add(activeClass);
+
+                // Update the last open section and the aside option
                 $oppenSection = $section;
+                $oppenSectionBtn = $sectionBtn;
             });
         });
     }
